b097ad65519359632338389ac97f2258b3c1c4d9
Brightmuk <be.rightmuk@gmail.com>
2019-04-27 02:44:41 +0300

    Adds styling to the layout to make it more appealing

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,7 +9,16 @@ export class QuotesComponent implements OnInit {
 
   quotes = [
     new Quotes('Fly, if you cant fly run, if you cant run walk,if you cant walk crawl .',
-  'Author:Myles Munroe',new Date(2019,3,2019)),
+      'Author: Myles Munroe',new Date()),
+    new Quotes('Get busy living or get busy dying.Life is not a test till you start failing','Author: Mark claine',
+        new Date()),
+    new Quotes('The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.'
+       ,'Author: Stephen King',new Date()),
+    new Quotes('Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
+       'Author: Mark Twaine',new Date()),
+    new Quotes('When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid.',
+       'Author: Audre leon', new Date()),
+
   ]
 
   deleteQuote(deleteit,index){
