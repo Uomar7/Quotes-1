98924701413224f855aa8ecfc990b90d8f0202f9
Brightmuk <be.rightmuk@gmail.com>
2019-04-29 08:39:45 +0300

    Adds functionality to like and dislike button to display number of likes and dislikes

--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-votes',
+  selector: 'votes-box',
   templateUrl: './votes.component.html',
   styleUrls: ['./votes.component.css']
 })
 export class VotesComponent implements OnInit {
+  likes :number=0;
+  dislikes: number=0;
 
+  likeBtn(){
+    this.likes++;
+  }
+  dislikeBtn(){
+    this.dislikes++;
+  }
   constructor() { }
 
   ngOnInit() {
